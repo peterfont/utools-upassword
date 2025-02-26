e27482de51616fe166434be7cb8679ed50d0aa58
benedict <zrbenedict@itcom888.com>
2025-02-26 11:03:33 +0800

    feat: 对接中文

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,13 +1,18 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 
 const app = createApp(App)
 
-app.use(ElementPlus)
+// 使用中文配置
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+
 app.use(createPinia())
 app.use(router)
 
